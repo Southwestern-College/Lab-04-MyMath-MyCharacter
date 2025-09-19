5acbc99d8c4c181b2af95150e5122ec3b43c9e8f
Raul Soto <rsoto@swccd.edu>
2025-09-19 16:37:09 -0700

    Create MyCharacter.java

---
+++ MyCharacter.java
@@ -0,0 +1,64 @@
+/**
+ * A utility class that provides basic character-related operations.
+ * This class recreates simplified versions of methods from the Character class.
+ * 
+ * @author Your Name
+ */
+public class MyCharacter {
+
+    /**
+     * Returns true if the character is a letter (A-Z or a-z).
+     * @param ch the character to check
+     * @return true if ch is a letter, false otherwise
+     */
+    public static boolean isLetter(char ch) {
+        // TODO: implement this method
+    }
+
+    /**
+     * Returns true if the character is a digit (0-9).
+     * @param ch the character to check
+     * @return true if ch is a digit, false otherwise
+     */
+    public static boolean isDigit(char ch) {
+        // TODO: implement this method
+    }
+
+    /**
+     * Returns true if the character is an uppercase letter (A-Z).
+     * @param ch the character to check
+     * @return true if ch is uppercase, false otherwise
+     */
+    public static boolean isUpperCase(char ch) {
+        // TODO: implement this method
+    }
+
+    /**
+     * Returns true if the character is a lowercase letter (a-z).
+     * @param ch the character to check
+     * @return true if ch is lowercase, false otherwise
+     */
+    public static boolean isLowerCase(char ch) {
+        // TODO: implement this method
+    }
+
+    /**
+     * Converts the given character to uppercase if it is lowercase.
+     * Otherwise, returns the character unchanged.
+     * @param ch the character to convert
+     * @return the uppercase version of ch if it is lowercase, otherwise ch
+     */
+    public static char toUpperCase(char ch) {
+        // TODO: implement this method
+    }
+
+    /**
+     * Converts the given character to lowercase if it is uppercase.
+     * Otherwise, returns the character unchanged.
+     * @param ch the character to convert
+     * @return the lowercase version of ch if it is uppercase, otherwise ch
+     */
+    public static char toLowerCase(char ch) {
+        // TODO: implement this method
+    }
+}
