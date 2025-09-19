56f99f99243350cac129d7e393597b16c490e136
Raul Soto <rsoto@swccd.edu>
2025-09-19 16:38:09 -0700

    Create MyMath.java

---
+++ MyMath.java
@@ -0,0 +1,93 @@
+/**
+ * A utility class that provides basic mathematical operations.
+ * This class recreates simplified versions of methods from the Math class.
+ * 
+ * @author Your Name
+ */
+public class MyMath {
+
+    /** The constant PI. */
+    public static final double PI = 3.141592653589793;
+
+    /** The constant E. */
+    public static final double E = 2.718281828459045;
+
+    /**
+     * Returns the sum of two numbers.
+     * @param a the first number
+     * @param b the second number
+     * @return the sum of a and b
+     */
+    public static double add(double a, double b) {
+        // TODO: implement this method
+    }
+
+    /**
+     * Returns the difference when the second number is subtracted from the first.
+     * @param a the first number
+     * @param b the second number
+     * @return the result of a - b
+     */
+    public static double subtract(double a, double b) {
+        // TODO: implement this method
+    }
+
+    /**
+     * Returns the product of two numbers.
+     * @param a the first number
+     * @param b the second number
+     * @return the product of a and b
+     */
+    public static double multiply(double a, double b) {
+        // TODO: implement this method
+    }
+
+    /**
+     * Returns the quotient when the first number is divided by the second.
+     * If the divisor is zero, the program should terminate.
+     * @param a the numerator
+     * @param b the denominator
+     * @return the result of a / b
+     */
+    public static double divide(double a, double b) {
+        // TODO: implement this method
+    }
+
+    /**
+     * Returns the greater of two numbers.
+     * @param a the first number
+     * @param b the second number
+     * @return the greater of a or b
+     */
+    public static double max(double a, double b) {
+        // TODO: implement this method
+    }
+
+    /**
+     * Returns the smaller of two numbers.
+     * @param a the first number
+     * @param b the second number
+     * @return the smaller of a or b
+     */
+    public static double min(double a, double b) {
+        // TODO: implement this method
+    }
+
+    /**
+     * Returns true if the number is even.
+     * @param a the number to check
+     * @return true if a is even, false otherwise
+     */
+    public static boolean isEven(int a) {
+        // TODO: implement this method
+    }
+
+    /**
+     * Returns true if the number is odd.
+     * @param a the number to check
+     * @return true if a is odd, false otherwise
+     */
+    public static boolean isOdd(int a) {
+        // TODO: implement this method
+    }
+}
